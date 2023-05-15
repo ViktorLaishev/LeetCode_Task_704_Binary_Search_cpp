9159b894c9d526f1135825a9172fd87b65cbd3a8
Viktor Laishev <130911518+ViktorLaishev@users.noreply.github.com>
2023-05-15 19:00:58 +0200

    Update solution.cpp

--- solution.cpp
+++ solution.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public: 
     int search(vector<int>& nums, int target) {     
-        int left = 0, right = nums.size() - 1;    
+        int left = 0, right = nums.size() - 1;     
         while (left <= right) {   
             int mid = left + (right - left) / 2;  
             if (nums[mid] == target) return mid;
